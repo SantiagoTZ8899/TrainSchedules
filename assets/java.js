9a083310f256782ac235d266ab4bc91eb9aecdf0
Santiago Tolosa Zegarra <santiagot-z@santiagos-mbp.home>
2019-09-25 23:34:18 -0500

    intitialize firebase in js, and outlined app

---
+++ assets/java.js
@@ -0,0 +1,25 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBRhCqhTh-Jyprabxs9R74cznOImojqzuA",
+    authDomain: "train-13586.firebaseapp.com",
+    databaseURL: "https://train-13586.firebaseio.com",
+    projectId: "train-13586",
+    storageBucket: "",
+    messagingSenderId: "56955142852",
+    appId: "1:56955142852:web:cda13b728e55958c3cff91"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// firebase variable
+let db = firebase.database();
+
+// button for adding submited form
+
+
+// push train info to firebase database
+
+
+// update current page with submited form info
+
+
